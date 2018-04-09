ff6963e97e319cc254b2bb53fb3919f65506fc32
Bharath Vadlamannati <bharry29@gmail.com>
2018-04-08 23:02:03 -0700

    Revert "Reverting changes"
    This reverts commit bedc62e8a146531560337435b3da3b4a45799e1c.

--- src/main/java/com/bharath/rulesapp/TestRules.java
+++ src/main/java/com/bharath/rulesapp/TestRules.java
@@ -1,3 +1,8 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package com.bharath.rulesapp;
 
 import java.io.File;
@@ -77,9 +82,8 @@ public class TestRules {
                 System.out.println(p.toString());
             }
             
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
+        } 
     }
 }
