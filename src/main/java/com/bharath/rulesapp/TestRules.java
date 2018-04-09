bedc62e8a146531560337435b3da3b4a45799e1c
Bharath Vadlamannati <bharry29@gmail.com>
2018-04-08 22:59:23 -0700

    Reverting changes
    Reverting changes

--- src/main/java/com/bharath/rulesapp/TestRules.java
+++ src/main/java/com/bharath/rulesapp/TestRules.java
@@ -1,8 +1,3 @@
-/*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
 package com.bharath.rulesapp;
 
 import java.io.File;
@@ -82,8 +77,9 @@ public class TestRules {
                 System.out.println(p.toString());
             }
             
-        } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) {
             e.printStackTrace();
-        } 
+        }
     }
 }
